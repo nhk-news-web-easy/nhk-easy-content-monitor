e8966645acfd998c74348561202e684e1ad92509
Frederick-S <mao_xiaodan@hotmail.com>
2022-08-21 15:47:38 +0800

    fetch news

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nhk-news-web-easy/nhk-easy-content-monitor/nhk"
+	"time"
 )
 
 func HandleRequest(ctx context.Context) (string, error) {
-	return fmt.Sprintf("Hello lambda"), nil
+	now := time.Now().UTC()
+	startDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+	endDate := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, time.UTC)
+
+	_, err := nhk.FetchNews(startDate, endDate)
+
+	if err != nil {
+		return "error", err
+	}
+
+	return "ok", nil
 }
 
 func main() {
---
+++ nhk/news_fetcher.go
@@ -0,0 +1,56 @@
+package nhk
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const requestUrl = "https://nhk.dekiru.app/news"
+const isoFormat = "2022-01-01T00:00:00.000Z"
+
+type News struct {
+	NewsId          string
+	Title           string
+	TitleWithRuby   string
+	OutlineWithRuby string
+	Body            string
+	BodyWithoutRuby string
+	Url             string
+	M3u8Url         string
+	ImageUrl        string
+	PublishedAtUtc  time.Time
+}
+
+func FetchNews(startDate time.Time, endDate time.Time) ([]News, error) {
+	url := fmt.Sprintf(requestUrl+"?startDate=%s&endDate=%s", startDate.Format(isoFormat), endDate.Format(isoFormat))
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	var newsList []News
+
+	err = json.Unmarshal(body, &newsList)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newsList, nil
+}
